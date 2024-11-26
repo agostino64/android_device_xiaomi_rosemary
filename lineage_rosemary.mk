rosemary-devs/android_device_xiaomi_rosemary-4.19

rosemary: checkout fingerprints from V14.0.10.0.TFFMIXM
Signed-off-by: dread <dreadcreator5@gmail.com>

lineage_rosemary.mk
@@ -26,6 +26,6 @@ PRODUCT_MODEL := M2101K7BNY
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="rosemary-user 12 SP1A.210812.016 V14.0.8.0.TKLMIXM release-keys"
+    PRIVATE_BUILD_DESC="rosemary-user 12 SP1A.210812.016 V14.0.10.0.TFFMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V14.0.8.0.TKLMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V14.0.10.0.TFFMIXM:user/release-keys

agostino64/android_device_xiaomi_rosemary

lineage_rosemary.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := M2101K7BNY
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="rosemary-user 12 SP1A.210812.016 V14.0.7.0.TKLMIXM release-keys"
+    PRIVATE_BUILD_DESC="rosemary-user 12 SP1A.210812.016 V14.0.10.0.TFFMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/rosemary_global/rosemary:12/SP1A.210812.016/V14.0.7.0.TKLMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/rosemary_global/rosemary:12/SP1A.210812.016/V14.0.10.0.TFFMIXM:user/release-keys